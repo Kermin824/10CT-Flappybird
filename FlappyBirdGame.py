9f736869c9c49037b67afbd780f8f3437460ad59
Kermin824 <kelvin.shi@education.nsw.gov.au>
2026-04-24 13:01:58 +1000

    Update FlappyBirdGame.py

--- FlappyBirdGame.py
+++ FlappyBirdGame.py
@@ -2,7 +2,35 @@ import pygame
 from pygame.locals import *
 import random
 
-# Initiates PyGame module
+# Initiates PyGame module ready for use
+pygame.init()
+
+
+# Set screen dimensions
+screen_width = 800
+screen_height = 900
+
+# Creates the game screen
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Bird')
+
+
+# Main game Loop
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+    pygame.display.update()
+
+# Quits PyGame
+pygame.quit()import pygame
+from pygame.locals import *
+import random
+
+# Initiates PyGame module ready for use
 pygame.init()
 
 
